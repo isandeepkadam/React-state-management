c88f597bc2df7684740c7f29c4c7d7f1b51dd05a
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-01-02 08:59:52 +0530

    react query and location

---
+++ react-others-ts/react-query-location/src/store.tsx
@@ -0,0 +1,90 @@
+import { useQuery } from '@tanstack/react-query';
+
+import React, {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+  useCallback,
+  useMemo,
+} from 'react';
+
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+function usePokemonSource(): {
+  pokemon: Pokemon[] | null;
+  search: string;
+  setSearch: (search: string) => void;
+} {
+  const { data: pokemon } = useQuery<Pokemon[]>(
+    [],
+    () => fetch('/pokemon.json').then((res) => res.json()),
+    {
+      initialData: [],
+    }
+  );
+
+  type PokemonState = {
+    search: string;
+  };
+
+  type PokemonAction = { type: 'setSearch'; payload: string };
+
+  const [{ search }, dispatch] = useReducer(
+    (state: PokemonState, action: PokemonAction) => {
+      switch (action.type) {
+        case 'setSearch':
+          return { ...state, search: action.payload };
+      }
+    },
+    {
+      search: '',
+    }
+  );
+
+  const setSearch = useCallback((search: string) => {
+    dispatch({
+      type: 'setSearch',
+      payload: search,
+    });
+  }, []);
+
+  const filteredPokemon = useMemo(() => {
+    return pokemon
+      .filter((p) => p.name.toLowerCase().includes(search))
+      .slice(0, 20);
+  }, [pokemon, search]);
+
+  const sortedPokemon = useMemo(() => {
+    return [...filteredPokemon].sort((a, b) => a.name.localeCompare(b.name));
+  }, [filteredPokemon]);
+
+  return { pokemon: sortedPokemon, search, setSearch };
+}
+
+export function usePokemon() {
+  return useContext(PokemonContext)!;
+}
+
+const PokemonContext = createContext<
+  ReturnType<typeof usePokemonSource> | undefined
+>(undefined);
+
+export function PokemonProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <PokemonContext.Provider value={usePokemonSource()}>
+      {children}
+    </PokemonContext.Provider>
+  );
+}
